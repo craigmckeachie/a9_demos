1cf8dabab513db64d9437aba88938f488d2e476b
Craig McKeachie <craigdmckeachie@gmail.com>
2018-10-16 22:27:33 -0500

    form binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   template: `
       <h1>
-        Welcome to {{title}}!
+       Forms
       </h1>
-    <router-outlet></router-outlet>
+      <form [formGroup]="loginForm" (submit)="onSubmit()">
+        <input formControlName="username" type="text" name="username">
+        <br>
+        <input formControlName="password" type="text" name="password">
+        <br>
+        <button>Sign In</button>
+      </form>
+
+      <pre>
+        {{loginForm.value | json}}
+      </pre>
   `,
   styles: []
 })
-export class AppComponent {
-  title = 'playground';
+export class AppComponent implements OnInit {
+  loginForm: FormGroup;
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl()
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
 }
