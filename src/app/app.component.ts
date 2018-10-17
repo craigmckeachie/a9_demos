fb450b1ae2fd8d7ecb822d5ecc6731dc6fc3914b
Craig McKeachie <craigdmckeachie@gmail.com>
2018-10-17 14:31:48 -0500

    http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from './photo.service';
 
 @Component({
   selector: 'app-root',
   template: `
       <h1>
-        Welcome to {{title}}!
+        HTTP
       </h1>
-    <router-outlet></router-outlet>
+      <div *ngFor="let photo of photos">
+      <img [src]="photo.thumbnailUrl" alt="">
+      {{photo.title}}
+      </div>
+
   `,
   styles: []
 })
-export class AppComponent {
-  title = 'playground';
+export class AppComponent implements OnInit {
+  photos: any[];
+  ngOnInit(): void {
+    this.photoService.getAll().subscribe(data => (this.photos = data));
+  }
+  constructor(private photoService: PhotoService) {}
 }
