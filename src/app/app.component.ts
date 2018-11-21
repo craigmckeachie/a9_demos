37f48a68ab8f79de4bbe58bd7a957d6c06d413c4
Craig McKeachie <craigdmckeachie@gmail.com>
2018-11-20 19:37:42 -0500

    remove code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { PhotoService } from './photo.service';
 
 @Component({
   selector: 'app-root',
   template: `
-      <h1>
-        Photos
-      </h1>
-      <div *ngFor="let photo of photos">
-      <img [src]="photo.thumbnailUrl" alt="">
-      <p>{{photo.title}}</p>
-      </div>
 
   `,
   styles: []
 })
-export class AppComponent implements OnInit {
-  photos: any[];
-  ngOnInit(): void {
-    this.photoService.getAll().subscribe(data => (this.photos = data));
-  }
-  constructor(private photoService: PhotoService) {}
-}
+export class AppComponent {}
