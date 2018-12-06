3d5e17db72981554550c24d1eee11338521abb36
Craig McKeachie <craigdmckeachie@gmail.com>
2018-12-06 14:56:32 -0500

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+import { GrandchildAComponent } from './grandchild-a/grandchild-a.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildAComponent,
+    ChildBComponent,
+    GrandchildAComponent
   ],
   imports: [
     BrowserModule,
