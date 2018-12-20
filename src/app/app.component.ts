701a10edca927e27f6ec319816e1db903ce5e117
Craig McKeachie <craigdmckeachie@gmail.com>
2018-12-20 11:08:58 -0500

    improve nav format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-      <ul>
-        <li routerLinkActive="active" ><a [routerLink]="['/home']">Home</a></li>
-        <li routerLinkActive="active"><a [routerLink]="['/about']">About</a></li>
-        <li routerLinkActive="active"><a [routerLink]="['/contact']">Contact</a></li>
-      </ul>
+    <nav>
+      <a routerLinkActive="active" [routerLink]="['/home']">Home</a> |
+      <a routerLinkActive="active" [routerLink]="['/about']">About</a> |
+      <a routerLinkActive="active" [routerLink]="['/contact']">Contact</a>
+    </nav>
     <router-outlet></router-outlet>
   `,
   styles: []
