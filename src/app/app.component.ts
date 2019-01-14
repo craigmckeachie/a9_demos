17aafad1122eda2f8d60633a50044a101aa0dfb3
Craig McKeachie <craigdmckeachie@gmail.com>
2019-01-14 15:24:12 -0500

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,25 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <form #signinForm="ngForm" (submit)="onSubmit(signinForm)">
-      <input type="text" ngModel name="username" placeholder="username" /><br />
+      <input
+        type="text"
+        placeholder="username"
+        ngModel
+        #username="ngModel"
+        name="username"
+        required
+        minlength="3"
+      /><br />
+      <pre>
+        {{ username.errors | json }}
+      </pre
+      >
+      <br />
+      Dirty: {{ username.dirty | json }} <br />
+      Touched: {{ username.touched | json }} <br />
       <input type="text" ngModel name="password" placeholder="password" /><br />
       <button>Sign In</button>
     </form>
-    <pre>
-      {{ signinForm.value | json }}
-    </pre
-    >
   `,
   styles: []
 })
