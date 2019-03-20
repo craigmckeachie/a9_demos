dccb9d18439b2aa40934b437e106c5f2bcd37920
Craig McKeachie <craigdmckeachie@gmail.com>
2019-03-19 21:42:40 -0400

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Observer, interval } from 'rxjs';
+import { map, tap, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -14,9 +15,16 @@ export class AppComponent implements OnInit {
       complete: () => console.log('completed'),
       error: e => console.log(e)
     };
-    const subscription = observable$.subscribe(observer);
-    setTimeout(() => {
-      subscription.unsubscribe();
-    }, 5000);
+
+    // const observableCommingOutOfThePipe$ = observable$.pipe(
+    //   // tap(x => console.log(x)),
+    //   // map(x => x * 10)
+    //   filter(x => x % 2 === 0)
+    // );
+    // observableCommingOutOfThePipe$.subscribe(observer);
+
+    interval(1000)
+      .pipe(filter(x => x % 2 === 0))
+      .subscribe(observer);
   }
 }
