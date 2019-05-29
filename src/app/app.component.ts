69697da5372f81e9e37654b55de31cb381954d35
Craig McKeachie <craigdmckeachie@gmail.com>
2019-05-29 17:16:00 -0400

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   template: `
-    <div class="example-boundary">
-      <div class="example-box" cdkDragBoundary=".example-boundary" cdkDrag>
-        Drag me around
+    <div cdkDropList class="example-list" (cdkDropListDropped)="drop($event)">
+      <div class="example-box" *ngFor="let movie of movies" cdkDrag>
+        {{ movie }}
       </div>
     </div>
   `,
   styles: []
 })
-export class AppComponent {}
+export class AppComponent {
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
+}
